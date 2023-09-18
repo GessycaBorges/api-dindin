ecf2818a2aac8b923a7f6398765edccd9bd22b5a
JulianaT93 <juliana.toguti@gmail.com>
2023-09-18 17:10:33 -0300

    Criação pasta intermediário e arquivo autenticação

---
+++ src/intermediarios/autenticacao.js
@@ -0,0 +1,37 @@
+const pool = require('../conexao');
+const jwt = require('jsonwebtoken');
+const senhaJwt = require('../senhaJwt');
+
+const verificarUsuarioLogado = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({ mensagem: 'Não autorizado' });
+    };
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const { id } = jwt.verify(token, senhaJwt);
+
+        const { rows, rowCount } = await pool.query(
+            'select * from usuarios where id = $1',
+            [id]
+        );
+
+        if (rowCount < 1) {
+            return res.status(401).json({ mensagem: 'Não autorizado' });
+        };
+
+        const { senha, ...usuario } = rows[0];
+
+        req.usuario = usuario;
+
+        next();
+    } catch (error) {
+        console.error(message);
+        return res.status(401).json({ mensagem: 'Não autorizado' });
+    };
+}
+
+module.exports = verificarUsuarioLogado;
